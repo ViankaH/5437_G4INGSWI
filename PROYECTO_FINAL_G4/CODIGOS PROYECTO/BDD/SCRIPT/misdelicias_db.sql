97f2150a0268db5ab625bf36157c045a586d17b1
Marlon Cevallos <mbcevallos3@espe.edu.ec>
2022-07-04 19:35:05 -0500

    BDD - Script

---
+++ PROYECTO_FINAL_G4/CODIGOS PROYECTO/BDD/SCRIPT/misdelicias_db.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-07-2022 a las 02:29:32
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `misdelicias_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(200) NOT NULL,
+  `Correo` varchar(200) NOT NULL,
+  `Direccion` varchar(200) NOT NULL,
+  `Usuario` varchar(50) NOT NULL,
+  `Contraseña` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`Id`, `Nombre`, `Correo`, `Direccion`, `Usuario`, `Contraseña`) VALUES
+(4, 'Marlon Cevallos', 'mbcevallos3@espe.edu.ec', 'La pulida casa 5', 'Marlon', '12345'),
+(5, 'prueba', 'lñkñl1', 'kljlkjlk', 'jlkjlk', 'kljlk'),
+(6, 'efdsd', 'kljljlk', 'jklj', 'lkj', 'lkj'),
+(7, 'estefy', 'este@hotmail.com', 'san carlos', 'estefy', '12345'),
+(8, '', '', '', '', ''),
+(9, 'WLADIMIR', 'A@HOTMAIL.COM', 'QUITO ', 'WLADI', '12345');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
